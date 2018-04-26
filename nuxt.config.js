d30df64a3ad241ae1699fdd534e992f65c7aed03
hagishi <hagishi.c@gmail.com>
2018-04-26 11:07:07 +0900

    Add ga #3

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,35 +5,38 @@ module.exports = {
 
   css: [
     // global cssを読み込む
-    { src: '~assets/sass/app.scss', lang: 'scss' }
+    {src: '~assets/sass/app.scss', lang: 'scss'}
     // { src: 'bulma/bulma.sass', lang: 'sass' }
   ],
   mode: 'spa',
   head: {
     title: 'yeah',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'Nuxt.js project'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: {color: '#eefe7d'},
   /*
   ** Build configuration
   */
-  plugins: ['~/plugins/i18n.js'],
+  plugins: [
+    '~/plugins/i18n.js',
+    {src: '~/plugins/ga.js', ssr: false},
+  ],
   build: {
     /*
     ** Run ESLint on save
     */
     vendor: ['axios', 'vue-i18n'],
-    extend (config, { isDev, isClient }) {
+    extend(config, {isDev, isClient}) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
